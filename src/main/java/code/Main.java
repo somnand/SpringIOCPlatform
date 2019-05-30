72f3dff4ad1d46eb4ff68f759df9fad43829cdb6
Somsurya Nanda <somsuryananda@gmail.com>
2019-05-31 03:23:11 +0530

    Adding Spring XML configuration. Used 4.3 as the base.

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -1,16 +1,30 @@
 package code;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
- * Hello world!
+ * Using Spring version of 5.1.7 
+ * XML XSD version is supported till 4.3 and thus the spring beans definition is 4.3
+ * <code>
+ * <beans xmlns="http://www.springframework.org/schema/beans"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.springframework.org/schema/beans
+   http://www.springframework.org/schema/beans/spring-beans-4.3.xsd">
+   </code>
+ * 
+ * @author Somsurya Nanda
  *
  */
 public class Main 
 {
     public static void main(String[] args )
     {
-        Vehicle vehicle = new Car();
+    	ApplicationContext context = new ClassPathXmlApplicationContext("application_components.xml");
+    	System.out.println("Using Spring IOC - Version 5.1.7");
+    	
+        Vehicle vehicle = (Vehicle)context.getBean("car");
         vehicle.drive();
-        vehicle = new Bike();
+        vehicle = (Vehicle)context.getBean("bike");
         vehicle.drive();
     }
 }
